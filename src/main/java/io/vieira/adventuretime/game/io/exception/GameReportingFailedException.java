e43f0c6e4b4d3ab19c1970d745bf75557be4a2c0
Vincent <vincent.vieira@supinfo.com>
2016-05-13 22:58:03 +0200

    fix(): fixes due to javadoc generation errors

--- src/main/java/io/vieira/adventuretime/game/io/exception/GameReportingFailedException.java
+++ src/main/java/io/vieira/adventuretime/game/io/exception/GameReportingFailedException.java
@@ -1,12 +1,13 @@
 package io.vieira.adventuretime.game.io.exception;
 
-import io.vieira.adventuretime.game.AdventureWorld;
 import io.vieira.adventuretime.game.io.write.AdventureReporter;
+
+import java.util.stream.Stream;
 
 /**
  * Exception thrown when game reporting fails.
  *
- * @see AdventureReporter#report(AdventureWorld)
+ * @see AdventureReporter#report(Stream)
  * @author <a href="mailto:vincent.vieira@supinfo.com">Vincent Vieira</a>
  */
 public class GameReportingFailedException extends RuntimeException {
--- src/main/java/io/vieira/adventuretime/game/routines/AutomaticMovementProvider.java
+++ src/main/java/io/vieira/adventuretime/game/routines/AutomaticMovementProvider.java
@@ -7,7 +7,7 @@ import java.util.concurrent.Callable;
 /**
  * Interface defining the behavior of an automated {@link io.vieira.adventuretime.game.AdventureWorld}.
  *
- * @see MovementProvider, the "manual" version.
+ * @see MovementProvider
  * @author <a href="mailto:vincent.vieira@supinfo.com">Vincent Vieira</a>
  */
 public interface AutomaticMovementProvider extends Callable<Void> {
@@ -15,7 +15,7 @@ public interface AutomaticMovementProvider extends Callable<Void> {
     /**
      * Moves automatically the adventurer passed as a parameter, using its internal configurer path.
      *
-     * @see Adventurer#getRemainingPath()
+     * @see Adventurer#remainingPath
      * @param adventurerName the adventurer name
      * @return a boolean specifying if the adventurer has remaining moves
      * @throws InterruptedException if the underlying {@link Callable} is interrupted.
